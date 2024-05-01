153b79f6a84820f512b342b62f66a2cb896b6d09
AthkiaAziza <athkiaaziza@gmail.com>
2024-05-01 11:00:30 +0600

    card added

---
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import { FaArrowRight } from "react-icons/fa6";
+
+const ServiceCard = ({service}) => {
+    const {title, img, price} = service;
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+                <img src={img} alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title text-2xl font-bold">{title}</h2>
+                <div className='flex items-center mt-4'>
+                <p className=' text-orange-500 text-2xl'>Price: ${price}</p>
+                <button className='text-orange-500 text-2xl'><FaArrowRight></FaArrowRight></button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+ServiceCard.propTypes = {
+    service:PropTypes.object
+}
+
+export default ServiceCard;
---
+++ src/pages/Home/Services/Services.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setServices(data));
+    }, [])
+
+
+    return (
+        <div>
+            <div className="text-center">
+                <h3 className="text-3xl text-orange-500 mb-5 font-bold">Services</h3>
+                <h3 className="text-black font-bold text-5xl">Our Service Area</h3>
+                <h3 className="text-xl mt-4">the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </h3>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {
+                    services.map(service => <ServiceCard
+                    key={service._id}
+                    service={service}
+                    ></ServiceCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+
+
+export default Services;
